bd3aa795b504c8ca325575f5fc5d797902fcc4b7
name <uio>
2024-03-01 13:50:29 +0300

    новое сообщение

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
 
-
+def f(x):
+    if x>0:
+        return 2*2
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
